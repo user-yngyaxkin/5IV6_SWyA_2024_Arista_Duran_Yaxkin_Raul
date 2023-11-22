43798ab040df5976278d6dcd01303d81d1edb783
user-yngyaxkin <129224713+user-yngyaxkin@users.noreply.github.com>
2023-11-22 00:23:55 -0600

    Add files via upload

---
+++ CIFRADO_RSA_MANITA/src/rsa_manita/RSA_Manita.java
@@ -0,0 +1,20 @@
+package rsa_manita;
+
+import GUI.MENU;
+
+/*
+ * @author Arista Duran Yaxkin Raul
+ */
+public class RSA_Manita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MENU menu = new MENU();
+        menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
+    }
+    
+}
+
